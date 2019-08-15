173cee3469eae23ce1913ce9727d670dac6845f8
Tom Elliott <xenomus@gmail.com>
2019-08-15 18:07:34 -0500

    character select finalized, began enemy select

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,33 +6,33 @@ var crumb;
 var dengar;
 var lumpy;
 var nien;
+var defender;
 
 // Initialize function
 function initialize() {
 
     // Set all initial characteristics of each object
+    dengar = {name: "Dengar", image: "assets/images/dengar.png", hp: 200, attack: 22, counter: 15, alive: true, player: false, enemy: ""};
 
-    dengar = {name: "Dengar", image: "assets/images/dengar.png", hp: 200, attack: 22, counter: 15, alive: true, player: false, enemy: false};
+    crumb = {name: "Salacious Crumb", image: "assets/images/crumb.jpg", hp: 100, attack: 15, counter: 10, alive: true, player: false, enemy: ""};
 
-    crumb = {name: "Salacious Crumb", image: "assets/images/crumb.jpg", hp: 100, attack: 15, counter: 10, alive: true, player: false, enemy: false};
+    lumpy = {name: "Lumpy", image: "assets/images/lumpy.jpg", hp: 250, attack: 18, counter: 14, alive: true, player: false, enemy: ""};
 
-    lumpy = {name: "Lumpy", image: "assets/images/lumpy.jpg", hp: 250, attack: 18, counter: 14, alive: true, player: false, enemy: false};
+    biggerluke = {name: "Bigger Luke", image: "assets/images/biggerluke.jpg", hp: 150, attack: 25, counter: 13, alive: true, player: false, enemy: ""};
 
-    biggerluke = {name: "Bigger Luke", image: "assets/images/biggerluke.jpg", hp: 150, attack: 25, counter: 13, alive: true, player: false, enemy: false};
+    nien = {name: "Nien Nunb", image: "assets/images/nien.jpg", hp: 190, attack: 20, counter: 20, alive: true, player: false, enemy: ""};
 
-    nien = {name: "Nien Nunb", image: "assets/images/nien.jpg", hp: 190, attack: 20, counter: 20, alive: true, player: false, enemy: false};
+    ackmena = {name: "Ackmena", image: "assets/images/ackmena.jpg", hp: 130, attack: 40, counter: 22, alive: true, player: false, enemy: ""};
 
-    ackmena = {name: "Ackmena", image: "assets/images/ackmena.jpg", hp: 130, attack: 40, counter: 22, alive: true, player: false, enemy: false};
+    defender = false;
 
     // Place all characters in boxes w/ green border at top with name, image and HP
-
     $("#dengar").html("Dengar<img class='char-img' src='" + dengar.image + "' />" + dengar.hp);
     $("#crumb").html("Salacious Crumb<img class='char-img' src='" + crumb.image + "' />" + crumb.hp);
     $("#lumpy").html("Lumpy<img class='char-img' src='" + lumpy.image + "' />" + lumpy.hp);
     $("#biggerluke").html("Bigger Luke<img class='char-img' src='" + biggerluke.image + "' />" + biggerluke.hp);
     $("#nien").html("Nien Nunb<img class='char-img' src='" + nien.image + "' />" + nien.hp);
     $("#ackmena").html("Ackmena<img class='char-img' src='" + ackmena.image + "' />" + ackmena.hp);
-
 };
 
 // Select player fighter
@@ -41,13 +41,12 @@ function initialize() {
     // When one character is clicked, set that character to be the player and all others to be enemies
 
     function setPlayer(player, en1, en2, en3, en4, en5) {
-
         player.player = true;
-        en1.enemy = true;
-        en2.enemy = true;
-        en3.enemy = true;
-        en4.enemy = true;
-        en5.enemy = true;
+        en1.enemy = "en1";
+        en2.enemy = "en2";
+        en3.enemy = "en3";
+        en4.enemy = "en4";
+        en5.enemy = "en5";
 
         $("#player").addClass("char-box");
         $("#player").html(player.name + "<img class='char-img' src='" + player.image + "' />" + player.hp);
@@ -66,13 +65,11 @@ function initialize() {
 
         $("#en5").addClass("enemy-box");
         $("#en5").html(en5.name + "<img class='char-img' src='" + en5.image + "' />" + en5.hp);
-
     };
 
     // Make function to clear out characters row
 
     function clearChars() {
-
         $("#dengar").html("");
         $("#dengar").removeClass();
         $("#crumb").html("");
@@ -85,79 +82,63 @@ function initialize() {
         $("#nien").removeClass();
         $("#ackmena").html("");
         $("#ackmena").removeClass();
-
     };
 
     // On character click, set player and enemeies
 
     $("#dengar").on("click", function() {
-
         if ((dengar.player==false)&&(crumb.player==false)&&(lumpy.player==false)&&(biggerluke.player==false)&&(nien.player==false)&&(ackmena.player==false)) {
-
             setPlayer(dengar, crumb, lumpy, biggerluke, nien, ackmena);
             clearChars();
-
         }
-
     });
 
     $("#crumb").on("click", function() {
-
         if ((dengar.player==false)&&(crumb.player==false)&&(lumpy.player==false)&&(biggerluke.player==false)&&(nien.player==false)&&(ackmena.player==false)) {
-
             setPlayer(crumb, dengar, lumpy, biggerluke, nien, ackmena);
             clearChars();
-
         }
-
     });
 
     $("#lumpy").on("click", function() {
-
         if ((dengar.player==false)&&(crumb.player==false)&&(lumpy.player==false)&&(biggerluke.player==false)&&(nien.player==false)&&(ackmena.player==false)) {
-
             setPlayer(lumpy, dengar, crumb, biggerluke, nien, ackmena);
             clearChars();
-
         }
-
     });
 
     $("#biggerluke").on("click", function() {
-
         if ((dengar.player==false)&&(crumb.player==false)&&(lumpy.player==false)&&(biggerluke.player==false)&&(nien.player==false)&&(ackmena.player==false)) {
-
             setPlayer(biggerluke, dengar, crumb, lumpy, nien, ackmena);
             clearChars();
-
         }
-
     });
 
     $("#nien").on("click", function() {
-
         if ((dengar.player==false)&&(crumb.player==false)&&(lumpy.player==false)&&(biggerluke.player==false)&&(nien.player==false)&&(ackmena.player==false)) {
-
             setPlayer(nien, dengar, crumb, lumpy, biggerluke, ackmena);
             clearChars();
-
         }
-
     });
 
     $("#ackmena").on("click", function() {
-
         if ((dengar.player==false)&&(crumb.player==false)&&(lumpy.player==false)&&(biggerluke.player==false)&&(nien.player==false)&&(ackmena.player==false)) {
-
             setPlayer(ackmena, dengar, crumb, lumpy, biggerluke, nien);
             clearChars();
-
         }
-
     });
 
-
 // Select enemy to attack
+
+    // Make a function to set enemy selected; move the char display from enemies to defender
+
+    function setDefender(defender, enLoc) {
+        if (defender==false) {
+            $("#" + enLoc).
+        }
+    };
+
+    $("en1").on("click", setDefender())
 
 // Player character attacks
 
